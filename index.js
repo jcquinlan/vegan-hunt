83e1d3eaf9d4c17d81e4fa2c1f6ec073169033cd
James Quinlan <quinlanjc@vcu.edu>
2016-04-14 00:01:45 -0400

    Added async Places call on card click

--- index.js
+++ index.js
@@ -82,16 +82,6 @@ function updateResultsNumber(){
     resultsNum.innerHTML = resultsList.length;
 }
 
-function getRestaurantDetails(location){
-    service.getDetails({ placeId: location }, callback);
-
-    function callback(place, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log(place);
-      }
-    }
-}
-
 
 // Store the results of the API call in a global array
 function storeResults(results) {
@@ -161,4 +151,94 @@ function clearMarkers(){
     // Reset the array of markers to make ready for the new ones
     markerList = [];
     console.log('Markers removed.');
+}
+
+
+
+
+///////////////////////////////////////////////////////////////////
+///////////////// Location JavaScript ////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+
+
+
+function createCard(location){
+    var col = document.createElement('div');
+    var card = document.createElement('div');
+    var info = document.createElement('div');
+    var additionalInfo = document.createElement('div');
+    var searchIcon = document.createElement('i');
+
+    searchIcon.classList.add('fa');
+    searchIcon.classList.add('fa-plus');
+    additionalInfo.classList.add('additional-info');
+
+    col.classList.add('col-lg-3');
+    col.classList.add('col-sm-4');
+    col.classList.add('col-xs-6');
+    card.classList.add('card');
+
+    card.setAttribute('id', location.place_id);
+    searchIcon.setAttribute('q', location.name + ' ' + location.vicinity);
+
+    searchIcon.addEventListener('click', selectCard, true);
+    card.addEventListener('click', openCard);
+
+    info.innerHTML = '<p class="location-name">' + location.name + '</p><p class="address">' + location.vicinity + '</p>';
+    additionalInfo.innerHTML = '<p class="rating">' + location.rating;
+
+    if(location.opening_hours && location.opening_hours.open_now){
+        info.innerHTML = info.innerHTML + '<p class="open-now"><i class="fa fa-clock-o" aria-hidden="true"></i>Open Now</p>';
+    }
+
+    card.appendChild(searchIcon);
+    card.appendChild(info);
+    card.appendChild(additionalInfo);
+    col.appendChild(card);
+    wrapper.appendChild(col);
+
+    console.log(location);
+}
+
+// Removes all the cards from the UI
+function destroyCards(){
+    while(wrapper.hasChildNodes()){
+        wrapper.removeChild(wrapper.childNodes[0]);
+        console.log(wrapper.childNodes.length);
+    }
+    resultsList = [];
+    console.log('Cards destroyed');
+}
+
+// Executed when a card is clicked
+function selectCard(event){
+    event.stopPropagation();
+    var q = event.target.getAttribute('q');
+    window.open('https://google.com/search?q=' + q);
+}
+
+// This function is run when a card is clicked and expanded
+function openCard(event){
+    var elem = this;
+    elem.classList.toggle('open');
+
+    if(elem.className.indexOf('open') != -1){
+        // Async get more details from Places library in Maps API
+        getRestaurantDetails(this.getAttribute('id'), elem);
+    }
+}
+
+// Async call to get full details on the restaurant a user clicked
+function getRestaurantDetails(location, element){
+    var additionalInfo = element.querySelector('.additional-info')
+    service.getDetails({ placeId: location }, callback);
+
+    function callback(place, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log(place);
+        additionalInfo.innerHTML += '<p>' + place.website + '</p>';
+        additionalInfo.innerHTML += '<p>' + place.formatted_phone_number + '</p>';
+      }
+    }
 }
--- locations.js
+++ locations.js
@@ -2,17 +2,26 @@ function createCard(location){
     var col = document.createElement('div');
     var card = document.createElement('div');
     var info = document.createElement('div');
+    var additionInfo = document.createElement('div');
     var searchIcon = document.createElement('i');
+
     searchIcon.classList.add('fa');
     searchIcon.classList.add('fa-plus');
+    additionalInfo.classList.add('additional-info');
+
     col.classList.add('col-lg-3');
     col.classList.add('col-sm-4');
     col.classList.add('col-xs-6');
     card.classList.add('card');
+
+    card.setAttribute('id', location.place_id);
     searchIcon.setAttribute('q', location.name + ' ' + location.vicinity);
+
     searchIcon.addEventListener('click', selectCard, true);
+    card.addEventListener('click', openCard);
 
     info.innerHTML = '<p class="location-name">' + location.name + '</p><p class="address">' + location.vicinity + '</p>';
+    additionInfo.innerHTML = '<p class="phone">' + location.
 
     if(location.opening_hours && location.opening_hours.open_now){
         info.innerHTML = info.innerHTML + '<p class="open-now"><i class="fa fa-clock-o" aria-hidden="true"></i>Open Now</p>';
@@ -41,4 +50,22 @@ function selectCard(event){
     event.stopPropagation();
     var q = event.target.getAttribute('q');
     window.open('https://google.com/search?q=' + q);
+}
+
+// This function is run when a card is clicked and expanded
+function openCard(event){
+    this.classList.toggle('open');
+    // Async get more details from Places library in Maps API
+    getRestaurantDetails(this.getAttribute('id'))
+}
+
+// Async call to get full details on the restaurant a user clicked
+function getRestaurantDetails(location){
+    service.getDetails({ placeId: location }, callback);
+
+    function callback(place, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        return place;
+      }
+    }
 }
